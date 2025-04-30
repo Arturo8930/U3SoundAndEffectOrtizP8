4a8391e106fc631a3a603a91f6388adb92c79662
Arturo Ortiz <753788@student.dusd.net>
2025-04-30 15:31:01 -0700

    U3 Sound And Effect (Part.5)
    Finished with the project; need to do the bonus features next.

--- P8OrtizU3SoundAndEffect/Assets/Scripts/SpawnManager.cs
+++ P8OrtizU3SoundAndEffect/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerControllerIII playerControllerIIIScript;
+    private int randomObstacle;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,9 @@ public class SpawnManager : MonoBehaviour
     void SpawnObstacle ()
     {
         if(playerControllerIIIScript.gameOver == false)
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        } 
     }
 }

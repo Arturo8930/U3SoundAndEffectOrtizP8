e0e7538695ff92c8ca3728f105a0e4811ecf2d8b
Arturo Ortiz <753788@student.dusd.net>
2025-05-14 14:05:27 -0700

    Challange3
    Tried it, but did not quite finish.

---
+++ P8OrtizU3SoundAndEffect/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerXScript;
+    private float leftBound = -50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerXScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If game is not over, move to the left
+        if (playerControllerXScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+    
+        if (transform.position.x < leftBound && gameObject.CompareTag("Bomb"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ P8OrtizU3SoundAndEffect/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] objectPrefabs;
+    private float startDelay = 2;
+    private float repeatRate= 2f;
+    private int bombObject;
+    private PlayerControllerX playerControllerXScript;
+    private Vector3 spawnPos = new Vector3(40, 13, 0);
+
+    private PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", startDelay, repeatRate);
+        playerControllerXScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Spawn obstacles
+    void SpawnObjects ()
+    {
+        // Set random spawn location and random object index
+        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        bombObject = Random.Range(0, objectPrefabs.Length);
+
+        // If game is still active, spawn new object
+        if (playerControllerXScript.gameOver == false)
+        {
+            Instantiate(objectPrefabs[bombObject], spawnPos, objectPrefabs[bombObject].transform.rotation);
+        }
+
+    }
+}
